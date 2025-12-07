27c0088700ed60a898e3b54fc190aa6541b29b1c
Ian <ian.warburton3000@gmail.com>
2025-12-07 19:38:08 +0000

    Refactor

--- Day-07/Part2.cs
+++ Day-07/Part2.cs
@@ -1,14 +1,17 @@
 var lines = File.ReadAllLines("input.txt");
-var splitters = lines.Select(line =>
-        line.Select((ch, i) => (ch, i)).Where(x => x.ch == '^').Select(x => x.i).ToHashSet()).ToArray();
 var cache = new Dictionary<(int, int), long>();
+var splitters = lines.Select(line =>
+    line.Select((ch, i) => (ch, i)).Where(x => x.ch == '^').Select(x => x.i).ToHashSet()).ToArray();
 
-Func<int, int, long> iterate = null!;
-iterate = (beam, index) =>
+Func<int, int, long> compute = null!, memoize = null!;
+compute = (beam, index) =>
     index == lines.Length ? 0 :
-        cache.TryGetValue((beam, index), out var cached) ? cached :
-            cache[(beam, index)] = splitters[index].Contains(beam)
-                ? new[] { beam - 1, beam + 1 }.Aggregate(1L, (acc, b) => acc + iterate(b, index + 1))
-                : iterate(beam, index + 1);
+        splitters[index].Contains(beam)
+            ? new[] { beam - 1, beam + 1 }.Aggregate(1L, (acc, b) => acc + memoize(b, index + 1))
+            : memoize(beam, index + 1);
 
-Console.WriteLine(iterate(lines[0].IndexOf('S'), 0) + 1);
+memoize = (beam, index) =>
+    cache.TryGetValue((beam, index), out var cached) ? cached :
+        cache[(beam, index)] = compute(beam, index);
+
+Console.WriteLine(memoize(lines[0].IndexOf('S'), 0) + 1);
